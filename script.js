b016dd2141bd3607fcd88fbe452bcb052ecc7ad2
Galih Setyawan <galih.kamvax032@gmail.com>
2023-08-14 21:43:36 +0700

    add: first message

---
+++ script.js
@@ -0,0 +1,10 @@
+const firstMessage = `
+Pilih Menu:
+1. Penjumlahan
+2. Penguranngan
+3. Pembagian
+4. Perkalian
+5. Pangkat 2
+6. Akar Pangkat 2
+7. Akar Pangkat 3`
+const menu = prompt(firstMessage)
