6650313e69a8cdefdd540ee0755904781b38a6b7
NaufanRikza <deathriver26@gmail.com>
2023-08-14 22:18:10 +0700

    feat:Penmabahan perkalian dan pembagian

--- script.js
+++ script.js
@@ -6,20 +6,41 @@ Pilih Menu:
 4. Perkalian
 5. Pangkat 2
 6. Akar Pangkat 2
-7. Akar Pangkat 3`
-const menu = prompt(firstMessage)
-console.log(menu)
+7. Akar Pangkat 3`;
+const menu = prompt(firstMessage);
+console.log(menu);
+
 switch (menu) {
-    case "1":
-        penjumlahan()
-        break
-    default:
-        alert('Masukan anda tidak terdapat pada menu')
+  case "1":
+    penjumlahan();
+    break;
+  case "2":
+    pengurangan();
+  case "3":
+    pembagian();
+  case "4":
+    perkalian();
+  default:
+    alert("Masukan anda tidak terdapat pada menu");
 }
 
 function penjumlahan() {
-    const x1 = prompt('Masukkan angka pertama')
-    const x2 = prompt('Masukkan angka kedua')
-    const total = parseInt(x1) + parseInt(x2)
-    alert(`Penjumlahan dari ${x1}+${x2}=${total}`)
+  const x1 = prompt("Masukkan angka pertama");
+  const x2 = prompt("Masukkan angka kedua");
+  const total = parseInt(x1) + parseInt(x2);
+  alert(`Penjumlahan dari ${x1}+${x2}=${total}`);
+}
+
+function pembagian() {
+  const angka1 = prompt("Masukkan angka pertama");
+  const angka2 = prompt("Masukkan angka kedua");
+  const pengurangan = parsefloat(angka1) / parseFloat(angka2);
+  alert(`Pembagian dari ${angka1}/${angka2}=${pengurangan}`);
+}
+
+function perkalian() {
+  const angka1 = prompt("Masukkan angka pertama");
+  const angka2 = prompt("Masukkan angka kedua");
+  const pengurangan = parseint(angka1) * parseInt(angka2);
+  alert(`Pembagian dari ${angka1}*${angka2}=${pengurangan}`);
 }
