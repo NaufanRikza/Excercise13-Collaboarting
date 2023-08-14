d79ac6f1e77be29b2e753171fce4308656706e50
Mikhail <mikhailyuwankho@gmail.com>
2023-08-14 22:10:58 +0700

    feat: penambahan case 7 dan 8

--- script.js
+++ script.js
@@ -10,16 +10,34 @@ Pilih Menu:
 const menu = prompt(firstMessage)
 console.log(menu)
 switch (menu) {
-    case "1":
-        penjumlahan()
-        break
-    default:
-        alert('Masukan anda tidak terdapat pada menu')
+  case "1":
+    penjumlahan()
+    break
+  case "7":
+    akarPangkat2()
+    break
+  case "8":
+    akarPangkat3()
+    break
+  default:
+    alert("Masukan anda tidak terdapat pada menu")
 }
 
 function penjumlahan() {
-    const x1 = prompt('Masukkan angka pertama')
-    const x2 = prompt('Masukkan angka kedua')
-    const total = parseInt(x1) + parseInt(x2)
-    alert(`Penjumlahan dari ${x1}+${x2}=${total}`)
+  const x1 = prompt("Masukkan angka pertama")
+  const x2 = prompt("Masukkan angka kedua")
+  const total = parseInt(x1) + parseInt(x2)
+  alert(`Penjumlahan dari ${x1}+${x2}=${total}`)
+}
+
+function akarPangkat2() {
+  const n = prompt("masukan angka yang ingin dipangkat 2")
+  let result = math.sqrt(n)
+  alert(`Akar pangkat 2 dari ${n} adalah ${result}`)
+}
+
+function akarPangkat3() {
+  const n = prompt("masukan angka yang ingin dipangkat 3")
+  let result = math.cbrt(n)
+  alert(`Akar pangkat 3 dari ${n} adalah ${result}`)
 }
