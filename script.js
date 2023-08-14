0be04f84d065a01ee8e4912b2c5eb7276a35442d
Galih Setyawan <galih.kamvax032@gmail.com>
2023-08-14 21:53:55 +0700

    add: penjumlahan function

--- script.js
+++ script.js
@@ -8,3 +8,18 @@ Pilih Menu:
 6. Akar Pangkat 2
 7. Akar Pangkat 3`
 const menu = prompt(firstMessage)
+console.log(menu)
+switch (menu) {
+    case "1":
+        penjumlahan()
+        break
+    default:
+        alert('Masukan anda tidak terdapat pada menu')
+}
+
+function penjumlahan() {
+    const x1 = prompt('Masukkan angka pertama')
+    const x2 = prompt('Masukkan angka kedua')
+    const total = parseInt(x1) + parseInt(x2)
+    alert(`Penjumlahan dari ${x1}+${x2}=${total}`)
+}
